684cfac6f72b25b8c05b1390e67374d6a6e7cbb1
Nicholas de Jong <ndejong@threatpatrols.com>
2025-03-05 12:21:19 +1000

    Apply Apache-2.0 license headers

--- src/threatpatrols_action/shared/lib/system_info.py
+++ src/threatpatrols_action/shared/lib/system_info.py
@@ -1,5 +1,5 @@
 #
-# Copyright [2023-2024] Threat Patrols Pty Ltd (https://www.threatpatrols.com)
+# Copyright [2025] Threat Patrols Pty Ltd (https://www.threatpatrols.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
